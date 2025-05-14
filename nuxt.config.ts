7adb35c1b606e44f98131edd014be77d2569ddc8
gkfat <gkgkdesign@gmail.com>
2025-05-14 11:41:17 +0800

    chore: 字體設定與 favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,16 +49,23 @@ export default defineNuxtConfig({
             ],
             link: [
                 {
-                    rel: 'apple-touch-icon', size: '180x180', href: '/apple-touch-icon.png',
+                    rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png',
                 },
                 {
-                    rel: 'icon', type: 'image/png', size: '32x32', href: '/favicon-32x32.png',
+                    rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png',
                 },
                 {
-                    rel: 'icon', type: 'image/png', size: '16x16', href: '/favicon-16x16.png',
+                    rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png',
+                },
+                {
+                    rel: 'icon', type: 'image/x-icon', href: '/favicon.ico', 
                 },
                 {
                     rel: 'manifest', href: '/site.webmanifest',
+                },
+                {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap',
                 },
             ],
         },
