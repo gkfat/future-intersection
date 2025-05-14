f82f8caaee48c6d3007617514f3f1edf0de3d05d
gkfat <gkgkdesign@gmail.com>
2025-05-14 11:53:29 +0800

    chore: update nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     srcDir: 'src/',
+    ssr: true,
 
     compatibilityDate: '2024-11-01',
     devtools: { enabled: false },
